6b789f1d86e68d9ee03d8761feb26ac31be8c4b5
Will Clarke <125552729+wclarke-pop@users.noreply.github.com>
2023-02-16 10:13:41 +0000

    feat: check missing title

---
+++ check-meta-title/main.php
@@ -0,0 +1,31 @@
+<?php
+
+if (!function_exists('str_contains')) {
+    function str_contains($haystack, $needle) {
+        return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+    }
+}
+
+
+$urls = [
+    'https://google.com'
+];
+
+
+foreach ($urls as $url) {
+    //* $url = "https://www.popcreative.co.uk/";
+
+    $handle = curl_init($url);
+    
+    curl_setopt_array($handle, [
+        CURLOPT_RETURNTRANSFER => true
+    ]);
+    
+    $response = curl_exec($handle);
+    
+    curl_close($handle);
+    
+    //* var_dump (str_contains($response, '<meta name="description" '));
+    if (str_contains ($response, '<title>') ) echo "pass" . "<br>";
+    else echo $url . "<br>";
+}
